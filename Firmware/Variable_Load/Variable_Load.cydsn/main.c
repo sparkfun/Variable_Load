12dae4479eb8436bb77cd26862561336d17e93f0
Robert Keck <37019627+keck9939@users.noreply.github.com>
2018-03-04 19:33:36 -0500

    Improve precision of current measurement. Add ability to set vMin and enable/disable load from terminal. Use internal reference for source voltage ADC (rather than 1/2 the supply voltage). This make it accurate, but limits the input voltage to about 20V with the 10:1 divider. Ideally the divider would be changed to 20:1.

--- Firmware/Variable_Load/Variable_Load.cydsn/main.c
+++ Firmware/Variable_Load/Variable_Load.cydsn/main.c
@@ -9,8 +9,8 @@
 #define KIN 1
 #define KID 10
 
-#define DEFAULT_I_LIM 0.0
-
+#define DEFAULT_I_LIM 0
+#define DEFAULT_V_MIN 2000
 
 /* Defines for SourceDMA */
 #define SourceDMA_BYTES_PER_BURST 2
@@ -20,12 +20,13 @@
 
 /* Variable declarations for SourceDMA */
 /* Move these variable declarations to the top of the function */
-#define ADCSAMPLES 64
+#define ADCSAMPLES 40
 static uint8 SourceDMA_Chan;
 static uint8 SourceDMA_TD[1];
 static uint16 SourceData[ADCSAMPLES];
 
 void DoPid();
+void OutputEnable(bool v);
 
 volatile static int32 systemTimer = 0;
 
@@ -34,22 +35,27 @@ volatile static int iLimit = DEFAULT_I_LIM;
 volatile static int vSource = 0;
 volatile static int iSource = 0;
 volatile static uint32 dt = 0;
-volatile static int vMin = 2000;
+volatile static int vMin = DEFAULT_V_MIN;
 
 bool enableOutput = false;
 
 int main(void)
 {
-	int i;
-    int vAve;
-    
-    CyGlobalIntEnable; /* Enable global interrupts. */
+	static int i;
+	static int vAve;
+	static char t;
+	static char buff[64];
+	static uint8 inBuff[64];
+	static char floatBuff[64];
+	static uint8_t incCharIndex = 0;
+
+	CyGlobalIntEnable; /* Enable global interrupts. */
 
 	// Create our timing "tick" variables. These are used to record when the last
 	// iteration of an action in the main loop happened and to trigger the next
 	// iteration when some number of clock ticks have passed.
 	int32_t SlowTick = 0;
-    
+
 	Gate_Drive_Tune_Start();
 	Offset_Start();
 	Offset_Gain_Start();
@@ -57,16 +63,11 @@ int main(void)
 	O_Buffer_Start();
 	Source_ADC_Start();
 	I_Source_ADC_Start();
-    
+
 	bool upPressed = false;
 	bool downPressed = false;
 	bool entPressed = false;
 	bool backPressed = false;
-
-	char buff[64];
-	uint8_t inBuff[64];
-	uint8_t floatBuff[64];
-	uint8_t incCharIndex = 0;
 
 	USBUART_Start(0, USBUART_5V_OPERATION);
 	CapSense_Start();
@@ -76,19 +77,19 @@ int main(void)
 	LCD_DisplayOn();
 	LCD_PrintString("Hello, world");
 
-    /* DMA Configuration for SourceDMA */
-    SourceDMA_Chan = SourceDMA_DmaInitialize(SourceDMA_BYTES_PER_BURST, SourceDMA_REQUEST_PER_BURST, 
-        HI16(SourceDMA_SRC_BASE), HI16(SourceDMA_DST_BASE));
-    SourceDMA_TD[0] = CyDmaTdAllocate();
-    CyDmaTdSetConfiguration(SourceDMA_TD[0], 2*ADCSAMPLES, SourceDMA_TD[0], CY_DMA_TD_INC_DST_ADR | CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetAddress(SourceDMA_TD[0], LO16((uint32)Source_ADC_SAR_WRK0_PTR), LO16((uint32)SourceData));
-    CyDmaChSetInitialTd(SourceDMA_Chan, SourceDMA_TD[0]);
-    CyDmaChEnable(SourceDMA_Chan, 1);
+	/* DMA Configuration for SourceDMA */
+	SourceDMA_Chan = SourceDMA_DmaInitialize(SourceDMA_BYTES_PER_BURST, SourceDMA_REQUEST_PER_BURST,
+		HI16(SourceDMA_SRC_BASE), HI16(SourceDMA_DST_BASE));
+	SourceDMA_TD[0] = CyDmaTdAllocate();
+	CyDmaTdSetConfiguration(SourceDMA_TD[0], 2 * ADCSAMPLES, SourceDMA_TD[0], CY_DMA_TD_INC_DST_ADR | CY_DMA_TD_AUTO_EXEC_NEXT);
+	CyDmaTdSetAddress(SourceDMA_TD[0], LO16((uint32)Source_ADC_SAR_WRK0_PTR), LO16((uint32)SourceData));
+	CyDmaChSetInitialTd(SourceDMA_Chan, SourceDMA_TD[0]);
+	CyDmaChEnable(SourceDMA_Chan, 1);
 	Source_ADC_StartConvert();
-    
+
 	init();
 
-	for(;;)
+	for (;;)
 	{
 		if (0u != USBUART_IsConfigurationChanged())
 		{
@@ -97,49 +98,41 @@ int main(void)
 				USBUART_CDC_Init();
 			}
 		}
-		
-		if(0u == CapSense_IsBusy())
+
+		if (0u == CapSense_IsBusy())
 		{
 			CapSense_UpdateEnabledBaselines();
 			CapSense_ScanEnabledWidgets();
 		}
-		
+
 		// Handle a press of the back key
 		if (CapSense_CheckIsWidgetActive(CapSense_BACK__BTN) && backPressed == false)
 		{
 			backPressed = true;
 			iLimit = DEFAULT_I_LIM;
-			Output_On_LED_Write(0);
-			enableOutput = false;
+            vMin = DEFAULT_V_MIN;
+            OutputEnable(false);
 		}
 		else if (!CapSense_CheckIsWidgetActive(CapSense_BACK__BTN) && backPressed == true)
 		{
 			backPressed = false;
 		}
-		
+
 		if (CapSense_CheckIsWidgetActive(CapSense_ENTER__BTN) && entPressed == false)
 		{
 			entPressed = true;
-			enableOutput = !enableOutput;
-			if (enableOutput)
-			{
-				Output_On_LED_Write(1);
-			}
-			else
-			{
-				Output_On_LED_Write(0);
-			}
+			OutputEnable(!enableOutput);
 		}
 		else if (!CapSense_CheckIsWidgetActive(CapSense_ENTER__BTN) && entPressed == true)
 		{
 			entPressed = false;
 		}
-		
+
 		if (CapSense_CheckIsWidgetActive(CapSense_DOWN__BTN) && downPressed == false)
 		{
 			downPressed = true;
 			if (iLimit < 10) iLimit = 0;
-			else if (iLimit < 101) {if (iLimit >= 10) iLimit -= 10;}
+			else if (iLimit < 101) { if (iLimit >= 10) iLimit -= 10; }
 			else if (iLimit < 501) iLimit -= 50;
 			else if (iLimit < 1001) iLimit -= 100;
 			else iLimit -= 500;
@@ -148,7 +141,7 @@ int main(void)
 		{
 			downPressed = false;
 		}
-		
+
 		if (CapSense_CheckIsWidgetActive(CapSense_UP__BTN) && upPressed == false)
 		{
 			upPressed = true;
@@ -162,8 +155,8 @@ int main(void)
 		{
 			upPressed = false;
 		}
-		
-		
+
+
 		// Fetch any waiting characters from the USB UART.
 		int charCount = USBUART_GetCount();
 		if (charCount > 0)
@@ -173,67 +166,83 @@ int main(void)
 			for (i = 0; i < charCount; i++)
 			{
 				floatBuff[incCharIndex++] = inBuff[i];
-				if (incCharIndex > 63) incCharIndex = 0;
+				if (incCharIndex > 63)
+					incCharIndex = 0;
 				if (inBuff[i] == '\r' ||
-						inBuff[i] == '\n')
+					inBuff[i] == '\n')
 				{
 					floatBuff[incCharIndex] = '\0';
 					break;
 				}
 			}
 		}
-		
-// Average the DMA data from the Source Voltage ADC
-        vAve = 0;
-        for (i = 0; i < ADCSAMPLES; i++)
-            vAve += SourceData[i];
 
-        vAve /= ADCSAMPLES;
-        
-	    vSource = 10*Source_ADC_CountsTo_mVolts(vAve);
-		
-        if (systemTimer - 200 > SlowTick)
+		// Average the DMA data from the Source Voltage ADC
+		vAve = 0;
+		for (i = 0; i < ADCSAMPLES; i++)
+			vAve += SourceData[i];
+
+		vSource = Source_ADC_CountsTo_mVolts(vAve/4); // 10*vAve/40
+
+		if (systemTimer - 200 > SlowTick)
 		{
 			SlowTick = systemTimer;
 			cls();
-			goToPos(1,1);
+			goToPos(1, 1);
 			putString("I Source:");
-			goToPos(1,2);
+			goToPos(1, 2);
 			putString("I Limit:");
-			goToPos(1,3);
+			goToPos(1, 3);
 			putString("V Source:");
-			goToPos(1,4);
+			goToPos(1, 4);
 			putString("V Min:");
-			goToPos(12,1);
+			goToPos(12, 1);
 			if (iSource < 0) iSource *= -1;
-			sprintf(buff, "%.3f", iSource/1000.0f);
+			sprintf(buff, "%6.3f", iSource / 1000.0f);
 			putString(buff);
-			goToPos(12,2);
-			sprintf(buff, "%.3f", iLimit/1000.0f);
+			goToPos(12, 2);
+			sprintf(buff, "%6.3f", iLimit / 1000.0f);
 			putString(buff);
-			goToPos(12,3);
-			sprintf(buff, "%.3f", vSource/1000.0f);
+			goToPos(12, 3);
+			sprintf(buff, "%6.3f", vSource / 1000.0f);
 			putString(buff);
-			goToPos(12,4);
-			sprintf(buff, "%.3f", vMin/1000.0f);
+			goToPos(12, 4);
+			sprintf(buff, "%6.3f", vMin / 1000.0f);
 			putString(buff);
-			
-			sprintf(buff, "I: %.2f V: %.2f", iLimit/1000.0f, vSource/1000.0f);
-			LCD_Position(0,0);
+
+			sprintf(buff, "I: %.2f V: %.2f", iLimit / 1000.0f, vSource / 1000.0f);
+			LCD_Position(0, 0);
 			LCD_PrintString(buff);
-			sprintf(buff, "Imeas: %.2f", iSource/1000.0f);
-			LCD_Position(1,0);
+			sprintf(buff, "Imeas: %.2f", iSource / 1000.0f);
+			LCD_Position(1, 0);
 			LCD_PrintString(buff);
-			
+
 
 			if (floatBuff[incCharIndex] == '\0')
 			{
-				fiLimit = atoff((char*)floatBuff);
+				sscanf(floatBuff, "%c%f", &t, &fiLimit);
+				switch (t)
+				{
+				case 'i':
+				case 'I':
+					if (fiLimit > 4.000 ||
+						fiLimit < 0.0) fiLimit = 0.0;
+					iLimit = 1000.0*fiLimit;
+					break;
+				case 'v':
+				case 'V':
+					vMin = 1000.0*fiLimit;
+					break;
+				case 'e':
+				case 'E':
+					OutputEnable(fiLimit == 1);
+					break;
+				default:
+					break;
+				}
+
 				memset(floatBuff, 1, 64);
 				incCharIndex = 0;
-				if (fiLimit > 4.000 ||
-						iLimit < 0.0) iLimit = 0.0;
-                iLimit = 1000.0*fiLimit;
 			}
 		}
 	}
@@ -241,57 +250,69 @@ int main(void)
 
 void I_Source_ADC_ISR1_EntryCallback()
 {
-    systemTimer++;
+	systemTimer++;
 	DoPid();
 }
 
 
 void DoPid()
 {
-	static int error = 0.0;
-	static int integral = 0.0;
+	static int error = 0;
+	static int integral = 0;
 	static int32_t iSourceRaw = 0;
 	static uint16_t grossSetPoint = 0;
 	static uint16_t fineSetPoint = 0;
-	static int setPoint = 0.0;
-    
+	static int setPoint = 0;
+
 	iSourceRaw = I_Source_ADC_GetResult32();
-	iSource = 10 * I_Source_ADC_CountsTo_mVolts(iSourceRaw);
+	iSource = I_Source_ADC_CountsTo_mVolts(10*iSourceRaw);
 
 	error = iLimit - iSource;
 	integral = integral + error;
-	setPoint = (KPN * iLimit)/KPD + (KIN * integral)/KID + 2000; // Use feed forward plus integral
-    if (setPoint < 0)
-        setPoint = 0;
-    
+	setPoint = (KPN * iLimit) / KPD + (KIN * integral) / KID + 2000; // Use feed forward plus integral
+	if (setPoint < 0)
+		setPoint = 0;
+
 	// setPoint is a voltage. We need to convert that
 	//  into an integer that can be fed into our DACs.
 	// First, find our grossSetPoint. This is a largish voltage that
 	//  represents a coarse 0-4V offset in 16mV steps.
-	grossSetPoint = (int)(setPoint/16);
+	grossSetPoint = (int)(setPoint / 16);
 	// We want to limit our gross offset to 255, since it's an 8-bit
 	//  DAC output.
 	if (grossSetPoint > 255)
-	    grossSetPoint = 255;
+		grossSetPoint = 255;
 	// Now, find the fineSetPoint. This is a 4mV step 8-bit DAC which
 	//  allows us to tune the set point a little more finely.
-	fineSetPoint = (setPoint - grossSetPoint*16)/4;
-    if (fineSetPoint > 255)
-	    fineSetPoint = 255;
+	fineSetPoint = (setPoint - grossSetPoint * 16) / 4;
+	if (fineSetPoint > 255)
+		fineSetPoint = 255;
 
 	// Finally, one last check: if the source voltage is below 2.0V,
 	//  the output is disabled, or the total power is greater than 15W,
 	//  we want to clear everything and zero the gate drive.
 	if ((vSource < vMin) ||
-			(enableOutput == false) ||
-			(vSource * iLimit > 15000000))
+		(vSource * iLimit > 15000000))
+		OutputEnable(false);
+
+	if (enableOutput == false)
 	{
 		error = 0;
 		integral = 0;
 		grossSetPoint = 0;
 		fineSetPoint = 0;
 	}
+
 	Offset_SetValue(grossSetPoint);
 	Gate_Drive_Tune_SetValue(fineSetPoint);
+}
+
+void OutputEnable(bool v)
+{
+	enableOutput = v;
+	if (enableOutput)
+		Output_On_LED_Write(1);
+	else
+		Output_On_LED_Write(0);
 }
 
